4a20599882bb7ac9cd3166585a2fd5e54c158cd3
Thomas M Kehrenberg <tmke8@posteo.net>
2023-12-04 18:47:26 +0100

    Fix the python test

--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -1,6 +1,8 @@
-import pytest
 import latex2mmlc
 
 
-def test_sum_as_string():
-    assert latex2mmlc.convert_latex(r"\int") == "2"
+def test_integral():
+    assert (
+        latex2mmlc.convert_latex(r"\int")
+        == '<math xmlns="http://www.w3.org/1998/Math/MathML" display="inline">\n<mo>∫</mo>\n</math>'
+    )
