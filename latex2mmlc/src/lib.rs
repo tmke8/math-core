4bcef25fa9d5564df43357ac6e7390778af746e3
Thomas M Kehrenberg <tmke8@posteo.net>
2024-01-06 23:40:18 +0100

    Add test for operator name with spaces

--- latex2mmlc/src/lib.rs
+++ latex2mmlc/src/lib.rs
@@ -143,6 +143,7 @@ mod tests {
             ("double_subscript", r"g_{\mu\nu}"),
             ("simple_accent", r"\dot{x}"),
             ("operator_name", r"\operatorname{sn} x"),
+            ("operator_name_with_spaces", r"\operatorname{ hel lo }"),
             ("simple_binomial_coefficient", r"\binom12"),
             ("stretchy_parentheses", r"\left( x \right)"),
             ("stretchy_one-sided_parenthesis", r"\left( x \right."),
