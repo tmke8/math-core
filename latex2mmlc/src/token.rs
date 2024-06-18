94536670c2024166d00c21045c0d332049b9c228
Thomas M Kehrenberg <tmke8@posteo.net>
2024-06-18 16:02:10 +0200

    Make the code a lot smaller by defining Tokens more cleverly

--- latex2mmlc/src/token.rs
+++ latex2mmlc/src/token.rs
@@ -4,6 +4,7 @@ use crate::attribute::{FracAttr, Style, TextTransform};
 use crate::ops::Op;
 
 #[derive(Debug, Clone, PartialEq, AsRefStr)]
+#[repr(u32)]
 pub enum Token<'source> {
     #[strum(serialize = "end of document")]
     EOF,
@@ -21,6 +22,8 @@ pub enum Token<'source> {
     Right,
     Middle,
     Paren(Op),
+    #[strum(serialize = "]")]
+    SquareBracketClose,
     #[strum(serialize = "{")]
     GroupBegin,
     #[strum(serialize = "}")]
@@ -65,7 +68,9 @@ pub enum Token<'source> {
     BigOp(Op),
     Letter(char),
     NormalLetter(char),
-    Number(&'source str, Op),
+    Number(&'source str),
+    NumberWithDot(&'source str),
+    NumberWithComma(&'source str),
     Function(&'static str),
     #[strum(serialize = r"\operatorname")]
     OperatorName,
@@ -86,5 +91,16 @@ impl Token<'_> {
             self,
             Token::Sqrt | Token::Frac(_) | Token::Binom(_) | Token::Transform(_)
         )
+    }
+
+    /// Returns `true` if `self` and `other` are of the same kind.
+    /// Note that this does not compare the content of the tokens.
+    pub(crate) fn is_same_kind(&self, other: &Token) -> bool {
+        // SAFETY: Because `Self` is marked `repr(u32)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u32` discriminant as its first
+        // field, so we can read the discriminant without offsetting the pointer.
+        let self_discr = unsafe { *<*const _>::from(self).cast::<u32>() };
+        let other_discr = unsafe { *<*const _>::from(other).cast::<u32>() };
+        self_discr == other_discr
     }
 }
