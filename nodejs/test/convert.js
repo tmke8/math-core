5a4c8f4f647b8ee7feee716ff343b9a226d0cfac
Thomas M Kehrenberg <tmke8@posteo.net>
2025-11-14 21:41:17 +0100

    Make the JS options actually optional (#642)
    closes #633

--- nodejs/test/convert.js
+++ nodejs/test/convert.js
@@ -3,11 +3,7 @@ import { assert } from "chai";
 
 describe("Convert Tests", function () {
   context("Simple commands", function () {
-    const converter = new LatexToMathML({
-      prettyPrint: "never",
-      xmlNamespace: false,
-      macros: new Map(),
-    });
+    const converter = new LatexToMathML({});
     it("should convert simple command correctly", function () {
       const latex = "x\\sum x";
       const displayStyle = false;
@@ -21,17 +17,15 @@ describe("Convert Tests", function () {
       const displayStyle = true;
       assert.equal(
         converter.convert_with_local_counter(latex, displayStyle),
-        "<math display=\"block\"><mi>x</mi><mo>∑</mo><mi>x</mi></math>",
+        '<math display="block"><mi>x</mi><mo>∑</mo><mi>x</mi></math>',
       );
     });
   });
   context("Local equation numbering", function () {
-    const converter = new LatexToMathML({
-      prettyPrint: "auto",
-      xmlNamespace: false,
-      macros: new Map(),
-    });
     it("should convert equation with local numbering", function () {
+      const converter = new LatexToMathML({
+        prettyPrint: "auto",
+      });
       const latex = "\\begin{align}x\\\\y\\end{align}";
       const displayStyle = true;
       const output = converter.convert_with_local_counter(latex, displayStyle);
@@ -45,8 +39,6 @@ describe("Convert Tests", function () {
   context("Global equation numbering", function () {
     const converter = new LatexToMathML({
       prettyPrint: "auto",
-      xmlNamespace: false,
-      macros: new Map(),
     });
     it("should convert equation with global numbering", function () {
       const latex = "\\begin{align}x\\\\y\\end{align}";
@@ -54,7 +46,10 @@ describe("Convert Tests", function () {
       const output = converter.convert_with_global_counter(latex, displayStyle);
       assert.include(output, "(1)");
       assert.include(output, "(2)");
-      const output2 = converter.convert_with_global_counter(latex, displayStyle);
+      const output2 = converter.convert_with_global_counter(
+        latex,
+        displayStyle,
+      );
       assert.include(output2, "(3)");
       assert.include(output2, "(4)");
     });
@@ -65,7 +60,10 @@ describe("Convert Tests", function () {
       const output = converter.convert_with_global_counter(latex, displayStyle);
       assert.include(output, "(1)");
       assert.include(output, "(2)");
-      const output2 = converter.convert_with_global_counter(latex, displayStyle);
+      const output2 = converter.convert_with_global_counter(
+        latex,
+        displayStyle,
+      );
       assert.include(output2, "(3)");
       assert.include(output2, "(4)");
     });
@@ -75,15 +73,26 @@ describe("Convert Tests", function () {
       const macros = new Map();
       macros.set("RR", "\\mathbb{R}");
       const converter = new LatexToMathML({
-        prettyPrint: "never",
-        xmlNamespace: false,
-        macros: macros,
+        macros,
       });
       const latex = "\\RR";
       const displayStyle = false;
       assert.equal(
         converter.convert_with_local_counter(latex, displayStyle),
         "<math><mi>ℝ</mi></math>",
+      );
+    });
+  });
+  context("Continue on error", function () {
+    it("should continue on error", function () {
+      const converter = new LatexToMathML({
+        continueOnError: true,
+      });
+      const latex = "\\asdf";
+      const displayStyle = false;
+      assert.equal(
+        converter.convert_with_local_counter(latex, displayStyle),
+        '<span class="math-core-error" title="0: Unknown command &quot;\\asdf&quot;."><code>\\asdf</code></span>',
       );
     });
   });
