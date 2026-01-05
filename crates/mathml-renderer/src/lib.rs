62d5acb3885c169534e244ab6769753ac03b8cac
Thomas M Kehrenberg <tmke8@posteo.net>
2026-01-05 19:58:41 +0100

    Add simple documentation to math_core_renderer_internal

--- crates/mathml-renderer/src/lib.rs
+++ crates/mathml-renderer/src/lib.rs
@@ -1,3 +1,37 @@
+//! Internal library for the `math-core` crate for rendering MathML.
+//!
+//! This library allows you to construct an AST representing MathML and then render it to a string.
+//!
+//! # Example
+//!
+//! ```rust
+//! use math_core_renderer_internal::ast::Node;
+//! use math_core_renderer_internal::symbol;
+//! use math_core_renderer_internal::attribute::{MathSpacing, LetterAttr};
+//!
+//! let ast = Node::Row {
+//!     nodes: &[
+//!         &Node::Underset {
+//!             target: &Node::Operator {
+//!                 op: symbol::N_ARY_SUMMATION.as_op(),
+//!                 attr: None,
+//!                 left: Some(MathSpacing::Zero),
+//!                 right: None,
+//!             },
+//!             symbol: &Node::IdentifierChar('i', LetterAttr::Default),
+//!         },
+//!         &Node::IdentifierChar('i', LetterAttr::Default),
+//!      ],
+//!      attr: None,
+//! };
+//!
+//! let mut output = String::new();
+//! ast.emit(&mut output, 0).unwrap();
+//! assert_eq!(
+//!     output,
+//!     "<mrow><munder><mo lspace=\"0\">∑</mo><mi>i</mi></munder><mi>i</mi></mrow>"
+//! );
+//! ```
 pub mod arena;
 pub mod ast;
 pub mod attribute;
